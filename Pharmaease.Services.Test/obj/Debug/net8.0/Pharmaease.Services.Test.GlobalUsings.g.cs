f2f496a4be5676944237fd069dfebeb0698fae7a
DanielAraujoFaria <crosscross2.706@gmail.com>
2024-10-31 18:52:58 -0300

    Global Usings

--- Pharmaease.Services.Test/obj/Debug/net8.0/Pharmaease.Services.Test.GlobalUsings.g.cs
+++ Pharmaease.Services.Test/obj/Debug/net8.0/Pharmaease.Services.Test.GlobalUsings.g.cs
@@ -6,4 +6,3 @@ global using global::System.Linq;
 global using global::System.Net.Http;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
-global using global::Xunit;
